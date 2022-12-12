ce9ab4c5de80c583581f5658506e55324793b4cb
YogitaDevi <yogita.gry@gmail.com>
2022-12-12 17:39:38 +0530

    Find_MaxFloatNumber

---
+++ UC2_MaxFloat/MaxFloatNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_MaxFloat
+{
+    public class MaxFloatNumber
+    {
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if(firstValue.CompareTo(secondValue)>0 && firstValue.CompareTo(thirdValue)>0)
+            {
+                return firstValue;
+            }
+            if(secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if(thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("first Number, second Number, third Number are same");
+        }
+    }
+}
---
+++ UC2_MaxFloat/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UC2_MaxFloat
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Float number : ");
+            double output = MaxFloatNumber.MaximumFloatNumber(11.22, 22.0, 67.5);
+            Console.WriteLine(output);
+        }
+    }
+}
