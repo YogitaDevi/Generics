ee29e5e1aeba566d966678632cf3d42179e7fae0
YogitaDevi <yogita.gry@gmail.com>
2022-12-12 17:11:42 +0530

    Find_MaxNumber

---
+++ UC1_MaxIntegers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace UC1_MaxIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number: ");
+
+            int output = MaxNumberCheck.MaximunIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
