079d5613f880f98ca1195a22625c35b79c815a6d
YogitaDevi <yogita.gry@gmail.com>
2022-12-13 18:49:58 +0530

    [yogita]_FindMaxString

---
+++ UC3_MaxString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC3_MaxString
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Find Max String : ");
+            string stringOutput = MaxStringCheck.MaximumString("Apple","Peach","Banana");
+            Console.WriteLine(stringOutput);
+        }
+
+    }
+}
